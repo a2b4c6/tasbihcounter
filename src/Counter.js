ee3132aef55245a30f400dba8c24db44c5f61993
a2b4c6 <66155039+a2b4c6@users.noreply.github.com>
2020-05-31 15:08:02 +0100

    Implemented basic storage system

--- src/Counter.js
+++ src/Counter.js
@@ -3,21 +3,23 @@ import React, { Component } from 'react';
 class Counter extends Component {
 constructor(props) {
     super (props);
+
+    // initialize state
     this.state = { 
         dhikrCount: 0, 
         hasanatCount: 0 
     };
 }
 
-
+// increase state value
 increment = () => {
     this.setState({
-        dhikrCount: this.state.dhikrCount + 1,
-        hasanatCount: this.state.hasanatCount + 10
+        dhikrCount: parseInt(this.state.dhikrCount)+ 1,
+        hasanatCount: parseInt(this.state.hasanatCount) + 10
     });
 };
 
-
+// decrease state value
 decrement = () => {
     this.setState({
         dhikrCount: this.state.dhikrCount - 1,
@@ -25,12 +27,34 @@ decrement = () => {
     });
 };
 
+// reset state value
 reset = () => {
     this.setState({
         dhikrCount: 0,
         hasanatCount: 0
     });
 };
+
+
+
+// load saved state
+componentWillMount = () => {
+    console.log("component loaded test");
+    this.state.dhikrCount = localStorage.getItem("dhikrCount", parseInt(this.state.dhikrCount));
+    this.state.hasanatCount = localStorage.getItem("hasanatCount", parseInt(this.state.hasanatCount));
+    
+}
+
+
+
+// save state when updating
+componentDidUpdate = () => {
+    console.log("component updated test");
+    localStorage.setItem("dhikrCount", parseInt(this.state.dhikrCount));
+    localStorage.setItem("hasanatCount", parseInt(this.state.hasanatCount));
+}
+
+
 
     render(){
         return (
